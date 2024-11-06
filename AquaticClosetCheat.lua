b7eabb1f7f64d67b631df6faff5a82ed4c32e745
NexoSerKat <eloisakillermeow@gmail.com>
2024-11-06 20:24:27 +0800

    Update AquaticClosetCheat.lua

--- AquaticClosetCheat.lua
+++ AquaticClosetCheat.lua
@@ -85,3 +85,35 @@ local function randomizeHitbox(player)
         hrp.Anchored = false
     end
 end
+
+-- Motion blur 
+local camera = workspace.CurrentCamera
+local blurAmount = 15 --Select your amount--
+local blurAmplifier = 5 --If you know yk--
+local lastVector = camera.CFrame.LookVector
+
+local motionBlur = Instance.new("BlurEffect", camera)
+
+local runService = game:GetService("RunService")
+
+workspace.Changed:Connect(function(property)
+ if property == "CurrentCamera" then
+  print("Changed")
+  local camera = workspace.CurrentCamera
+  if motionBlur and motionBlur.Parent then
+   motionBlur.Parent = camera
+  else
+   motionBlur = Instance.new("BlurEffect", camera)
+  end
+ end
+end)
+
+runService.Heartbeat:Connect(function()
+ if not motionBlur or motionBlur.Parent == nil then
+  motionBlur = Instance.new("BlurEffect", camera)
+ end
+ 
+ local magnitude = (camera.CFrame.LookVector - lastVector).magnitude
+ motionBlur.Size = math.abs(magnitude)*blurAmount*blurAmplifier/2
+ lastVector = camera.CFrame.LookVector
+end)
